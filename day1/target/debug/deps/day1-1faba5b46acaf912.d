edf24a2fb1fb17a5f6fd8018cb935a2e3dfe7e41
meliomano <elioperez.conde@gmail.com>
2022-12-23 12:13:42 +0100

    Remove 'day/target/' directory from tracked files

--- day1/target/debug/deps/day1-1faba5b46acaf912.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/elio.perez/workdir/advent_of_code_2022/day1/target/debug/deps/day1-1faba5b46acaf912: src/main.rs
-
-/mnt/c/Users/elio.perez/workdir/advent_of_code_2022/day1/target/debug/deps/day1-1faba5b46acaf912.d: src/main.rs
-
-src/main.rs:
